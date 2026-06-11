41f48d23f872949eb1a7c1061f082c9f7bae307e
fabriziocpa <fabrizio.contreras.peralta@gmail.com>
2026-06-10 22:15:45 -0500

    refactor: reorganize imports and update entity field names for consistency

--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -1,41 +1,35 @@
-import 'package:mockito/annotations.dart';
-
-import 'package:app_mobile/features/announcements/domain/usecases/get_announcements_usecase.dart';
-import 'package:app_mobile/features/announcements/domain/usecases/get_announcement_by_id_usecase.dart';
-import 'package:app_mobile/features/announcements/domain/usecases/get_announcements_by_priority_usecase.dart';
-import 'package:app_mobile/features/announcements/domain/usecases/get_announcements_by_creator_usecase.dart';
-import 'package:app_mobile/features/announcements/domain/usecases/create_announcement_usecase.dart';
-import 'package:app_mobile/features/announcements/domain/usecases/update_announcement_usecase.dart';
-import 'package:app_mobile/features/announcements/domain/usecases/delete_announcement_usecase.dart';
-import 'package:app_mobile/features/announcements/domain/usecases/get_comments_usecase.dart';
-import 'package:app_mobile/features/announcements/domain/usecases/create_comment_usecase.dart';
-import 'package:app_mobile/features/announcements/domain/usecases/delete_comment_usecase.dart';
-
-import 'package:app_mobile/features/events/domain/usecases/get_events_usecase.dart';
-import 'package:app_mobile/features/events/domain/usecases/create_event_usecase.dart';
-import 'package:app_mobile/features/events/domain/usecases/update_event_usecase.dart';
-import 'package:app_mobile/features/events/domain/usecases/delete_event_usecase.dart';
-
-import 'package:app_mobile/features/iam/domain/usecases/sign_in_usecase.dart';
-import 'package:app_mobile/features/iam/domain/usecases/sign_up_usecase.dart';
-import 'package:app_mobile/features/iam/domain/usecases/sign_out_usecase.dart';
-import 'package:app_mobile/features/iam/domain/usecases/join_company_usecase.dart';
-
-import 'package:app_mobile/features/company/domain/usecases/create_company_usecase.dart';
-import 'package:app_mobile/features/company/domain/usecases/update_company_usecase.dart';
-import 'package:app_mobile/features/company/domain/usecases/get_company_by_user_id_usecase.dart';
-
-import 'package:app_mobile/features/profile/domain/usecases/get_profile_usecase.dart';
-import 'package:app_mobile/features/profile/domain/usecases/update_profile_usecase.dart';
-import 'package:app_mobile/features/profile/domain/usecases/get_company_members_usecase.dart';
-
+import 'package:app_mobile/core/auth/token_store.dart';
 import 'package:app_mobile/features/announcements/data/datasources/announcement_remote_datasource.dart';
 import 'package:app_mobile/features/announcements/data/datasources/comment_remote_datasource.dart';
-import 'package:app_mobile/features/events/data/datasources/event_remote_datasource.dart';
-import 'package:app_mobile/features/iam/data/datasources/iam_remote_datasource.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/create_announcement_usecase.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/create_comment_usecase.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/delete_announcement_usecase.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/delete_comment_usecase.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/get_announcement_by_id_usecase.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/get_announcements_by_creator_usecase.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/get_announcements_by_priority_usecase.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/get_announcements_usecase.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/get_comments_usecase.dart';
+import 'package:app_mobile/features/announcements/domain/usecases/update_announcement_usecase.dart';
 import 'package:app_mobile/features/company/data/datasources/company_remote_datasource.dart';
+import 'package:app_mobile/features/company/domain/usecases/create_company_usecase.dart';
+import 'package:app_mobile/features/company/domain/usecases/get_company_by_user_id_usecase.dart';
+import 'package:app_mobile/features/company/domain/usecases/update_company_usecase.dart';
+import 'package:app_mobile/features/events/data/datasources/event_remote_datasource.dart';
+import 'package:app_mobile/features/events/domain/usecases/create_event_usecase.dart';
+import 'package:app_mobile/features/events/domain/usecases/delete_event_usecase.dart';
+import 'package:app_mobile/features/events/domain/usecases/get_events_usecase.dart';
+import 'package:app_mobile/features/events/domain/usecases/update_event_usecase.dart';
+import 'package:app_mobile/features/iam/data/datasources/iam_remote_datasource.dart';
+import 'package:app_mobile/features/iam/domain/usecases/join_company_usecase.dart';
+import 'package:app_mobile/features/iam/domain/usecases/sign_in_usecase.dart';
+import 'package:app_mobile/features/iam/domain/usecases/sign_out_usecase.dart';
+import 'package:app_mobile/features/iam/domain/usecases/sign_up_usecase.dart';
 import 'package:app_mobile/features/profile/data/datasources/profile_remote_datasource.dart';
-
+import 'package:app_mobile/features/profile/domain/usecases/get_company_members_usecase.dart';
+import 'package:app_mobile/features/profile/domain/usecases/get_profile_usecase.dart';
+import 'package:app_mobile/features/profile/domain/usecases/update_profile_usecase.dart';
+import 'package:mockito/annotations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 @GenerateMocks([
@@ -78,5 +72,7 @@ import 'package:shared_preferences/shared_preferences.dart';
   ProfileRemoteDataSource,
   // SharedPreferences
   SharedPreferences,
+  // Auth token storage
+  TokenStore,
 ])
 void main() {}
